55ffdd4055004f3be486b1d6f82990e9796e3eb0
Ronny Ojeda <ronnyojeda1996@gmail.com>
2018-03-26 23:27:18 -0430

    Se le agregaron las direcciones de los proyectos y se agrego el servicio  para la carga de interna de la pagina

--- js/controladores/controller.js
+++ js/controladores/controller.js
@@ -1,5 +1,27 @@
-app.controller('controladorGeneral', ['$scope', function($scope){
+app.controller('controladorGeneral', ['$scope', '$anchorScroll','$location', function($scope,$anchorScroll,$location){
 	$scope.esconderboton=true;
+	$scope.menu=[
+	"Inicio",
+	"Acerca de mi",
+	"Proyectos",
+	"Contactame"
+	];
+	$scope.srA=function(id){
+		var nuevaId= "contenido" + id;
+		$location.hash(nuevaId);
+		$anchorScroll();
+	}
+	$scope.proyectos=[
+	{imagen:"cole-patrick-4576.jpg",
+	descripcion:"Una página de ropa tejida totalmente interactiva, asincrona, rápida y un manejo de datos excelente.",
+	urls:"https://Tejidoscrochet.000webhostapp.com"
+	},
+	{
+	imagen:"mariayronny.jpg",
+	descripcion:"Una pagina totalmente interactiva y amorosa",
+	urls: "https://ronnyojeda.github.io/portafolioRonny/"
+	}
+	];
 	$scope.habilidades = [
 		"htmllogo.png",
 		"css.png",
@@ -9,4 +31,7 @@ app.controller('controladorGeneral', ['$scope', function($scope){
 		"jquerylogo.png",
 		"ajaxlogo.png"
 			];
-}])
+	
+	
+
+}]);
